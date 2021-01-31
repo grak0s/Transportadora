3ffb90fa8a66050fc530e26c23261c05154e4e93
Mauro Rodriguez <64510491+grak0s@users.noreply.github.com>
2021-01-31 16:04:50 -0500

    Delete backend directory

--- backend/src/main/java/com/pruebaentrevista/EmpresaTransporte/repositories/VehiculoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pruebaentrevista.EmpresaTransporte.repositories;
-
-import com.pruebaentrevista.EmpresaTransporte.entities.Vehiculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VehiculoRepository extends JpaRepository<Vehiculo, Integer> {
-}
